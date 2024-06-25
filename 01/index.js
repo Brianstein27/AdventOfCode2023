2260c12b1e669ca5a23d381f42906f994b5c95ef
Brian Sauls <brianstein27@proton.me>
2024-06-25 16:03:20 +0200

    Create calculateSum function

--- 01/index.js
+++ 01/index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const fileContent = fs.readFileSync("./input.txt", "utf-8");
-const contentArray = fileContent.split("\n");
+const lines = fileContent.split("\n");
 
 const getFirstNumber = (line) => {
   for (let i = 0; i < line.length; i++) {
@@ -18,16 +18,17 @@ const getLastNumber = (line) => {
   }
 };
 
-let sum = 0;
+const calculateSum = (array) => {
+  let sum = 0;
 
-for (let line of contentArray) {
-  const firstNumber = getFirstNumber(line);
-  const lastNumber = getLastNumber(line);
-  const lineNumber = Number(firstNumber + lastNumber);
+  for (let line of array) {
+    const firstNumber = getFirstNumber(line);
+    const lastNumber = getLastNumber(line);
+    const lineNumber = Number(firstNumber + lastNumber);
 
-  if (!isNaN(lineNumber)) {
-    sum += lineNumber;
+    if (!isNaN(lineNumber)) {
+      sum += lineNumber;
+    }
   }
-}
-
-console.log(sum);
+  return sum;
+};
