84694559bc95f80abbab4ba6ebc1ef1f2c47d1c3
Brian Sauls <brianstein27@proton.me>
2024-06-26 14:08:22 +0200

    Change verbose numbers to digits

--- 01/index.js
+++ 01/index.js
@@ -1,6 +1,17 @@
 const fs = require("fs");
 const fileContent = fs.readFileSync("./input.txt", "utf-8");
 const lines = fileContent.split("\n");
+const numbers = {
+  one: "1",
+  two: "2",
+  three: "3",
+  four: "4",
+  five: "5",
+  six: "6",
+  seven: "7",
+  eight: "8",
+  nine: "9",
+};
 
 const getFirstNumber = (line) => {
   for (let i = 0; i < line.length; i++) {
@@ -18,13 +29,36 @@ const getLastNumber = (line) => {
   }
 };
 
+const convertToDigit = (line) => {
+  let newLine = line;
+
+  for (let i = 0; i < newLine.length; i++) {
+    for (let y = i + 1; y <= newLine.length; y++) {
+      const substring = newLine.substring(i, y);
+
+      if (Object.keys(numbers).includes(substring)) {
+        newLine = newLine.replace(substring, numbers[substring]);
+      }
+    }
+  }
+  return newLine;
+};
+
 const calculateSum = (array) => {
   let sum = 0;
 
   for (let line of array) {
-    const firstNumber = getFirstNumber(line);
-    const lastNumber = getLastNumber(line);
+    const cleanLine = convertToDigit(line);
+    const firstNumber = getFirstNumber(cleanLine);
+    const lastNumber = getLastNumber(cleanLine);
     const lineNumber = Number(firstNumber + lastNumber);
+
+    console.log("regular line", line);
+    console.log("clean line", cleanLine);
+    console.log("first number", firstNumber);
+    console.log("last number", lastNumber);
+    console.log("line number", lineNumber);
+    console.log("");
 
     if (!isNaN(lineNumber)) {
       sum += lineNumber;
@@ -32,3 +66,5 @@ const calculateSum = (array) => {
   }
   return sum;
 };
+
+console.log(calculateSum(lines));
