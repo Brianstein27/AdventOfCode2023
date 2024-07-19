48b1222ac88c868f459c8232c7cc60cf6094d66f
Brian Sauls <brianstein27@proton.me>
2024-07-19 14:52:19 +0200

    Finish day 2 exercise 1

---
+++ 02/index.js
@@ -0,0 +1,60 @@
+const fs = require("fs");
+const fileContent = fs.readFileSync("./input.txt", "utf-8");
+const gameStrings = fileContent.split("\n");
+gameStrings.pop();
+
+const requirements = { green: 13, red: 12, blue: 14 };
+
+const gameObjects = [];
+
+for (const game of gameStrings) {
+  let gameObject = {};
+  gameObject["sets"] = {};
+  let splitGame = game.split(":");
+  gameObject["id"] = Number(splitGame[0].split(" ")[1]);
+  gameObject["sets"] = [];
+
+  splitGame[1] = splitGame[1].split(";");
+  for (let index = 0; index < splitGame[1].length; index++) {
+    const colorObject = new Object();
+
+    for (const color of splitGame[1][index].split(",")) {
+      let colorSplit = color.split(" ");
+      colorSplit.shift();
+      const actualColor = colorSplit[1];
+      const amount = colorSplit[0];
+      colorObject[actualColor] = amount;
+    }
+    gameObject["sets"].push(colorObject);
+  }
+  gameObjects.push(gameObject);
+}
+
+function checkSet(set) {
+  let meetsRequirements = true;
+  for (const color in set) {
+    if (set[color] > requirements[color]) {
+      meetsRequirements = false;
+    }
+  }
+  return meetsRequirements;
+}
+
+function checkGame(game) {
+  let setsMeetRequirements = true;
+  for (const set of game.sets) {
+    if (!checkSet(set)) {
+      setsMeetRequirements = false;
+    }
+  }
+  return setsMeetRequirements;
+}
+
+let idSum = 0;
+for (const game of gameObjects) {
+  if (checkGame(game)) {
+    idSum += game.id;
+  }
+}
+
+console.log(idSum);
